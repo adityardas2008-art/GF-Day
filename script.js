38a248f2f6ef37bcbd83b2862841f7e51f44e9d9
adityardas2008-art <adityardas2008@gmail.com>
2026-07-30 10:31:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -365,67 +365,8 @@ behavior:"smooth"
 };
 
 }
-/* ---------------- */
-/* MUSIC */
-/* ---------------- */
 
-const song=document.getElementById("song");
-
-const playBtn=document.getElementById("playBtn");
-
-let playing=false;
-
-playBtn.onclick=function(){
-
-if(!playing){
-
-song.play();
-
-playBtn.innerHTML="⏸ Pause";
-
-playing=true;
-
-}
-
-else{
-
-song.pause();
-
-playBtn.innerHTML="▶ Play Our Song";
-
-playing=false;
-
-}
-
-}
-
-/* Final Celebration */
-
-document.getElementById("celebrate").onclick=function(){
-
-for(let i=0;i<200;i++){
-
-const heart=document.createElement("div");
-
-heart.className="heart";
-
-heart.innerHTML=["❤️","💖","💕","🌸"][Math.floor(Math.random()*4)];
-
-heart.style.left=Math.random()*100+"vw";
-
-heart.style.fontSize=(20+Math.random()*30)+"px";
-
-heart.style.animationDuration=(3+Math.random()*4)+"s";
-
-document.body.appendChild(heart);
-
-setTimeout(()=>heart.remove(),7000);
-
-}
-
-alert("Happy Girlfriend's Day, Khushu ❤️\n\nLove You Forever.\n- Aditya");
-
-}
+/* Final 
 /* ---------------- */
 /* FINAL */
 /* ---------------- */
